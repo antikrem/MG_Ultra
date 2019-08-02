397c27a943dbecbbb219932ccbf70ae83f64ee0e
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-08-03 04:22:36 +1000

    Finished implementing loading ents from load_tables

--- mg_ultra/system_loader.h
+++ mg_ultra/system_loader.h
@@ -20,7 +20,7 @@ ent [int] - ent direct
 ent [string] - ent table look up
 
 3. component adding
-[Component name] ...parameters
++[Component name] ...parameters
 
 */
 #ifndef __SYSTEM_LOADER__
@@ -31,15 +31,26 @@ ent [string] - ent table look up
 #include "registar.h"
 #include "os_kit.h"
 
-enum SL_LT_Target {
-	SL_LT_cycle,
-	SL_LT_counter,
-	SL_LT_callback,
-	SL_LT_immediate,
+enum TargetSpecification {
+	TaSp_noTarget,
+	TaSp_cycle,
+	TaSp_counter,
+	TaSp_callback,
+	TaSp_immediate,
+};
+
+struct TargetFullSpecification {
+	TargetSpecification target = TaSp_noTarget;
+	int cycle = 0;
+	string callback = "";
 };
 
 class SystemLoader : public System {
 	map<int, Entity*> cycleEnt;
+
+	//error stuff
+	int lineNumber;
+	string file;
 
 	//transfers ents from 
 	void transferBufferedEntsToGame(EntityPool* pool) {
@@ -96,19 +107,186 @@ class SystemLoader : public System {
 		return "";
 	}
 
+	//error stuff
+	void pushErrorLoading(string line, string id, string lex) {
+		string message = "LOAD: Error loading file: " + file + " at line " + to_string(lineNumber) + ". \n--> ";
+		message += "Got line: " + line + "\n--> ";
+		message += "Excepted: \"" + id +"\" ";
+		for (auto i : lex) {
+			if (i == 'i') {
+				message += "[int] ";
+			} 
+			else if (i == 'f') {
+				message += "[float] ";
+			}
+			else if (i == 's') {
+				message += "[string] ";
+			}
+			else {
+				message = "LOAD: Error loading file: " + file + " at line " + to_string(lineNumber) + ": invalid lex: " + lex;
+				break;
+			}
+		}
+		err::logMessage(message);
+	}
+
+	//takes a line and uses it to update the target
+	bool updateTarget(string line, TargetFullSpecification* spec) {
+		//conduct lexical analysis
+		str_kit::LexicalAnalysisResult result;
+
+		if ( result = str_kit::lexicalAnalysis(line, "@cycle", "i") ) {
+			if (result == str_kit::LAR_valid) {
+				spec->target = TaSp_cycle;
+				spec->cycle = str_kit::qStringToInt(line, 1);
+				return true;
+			}
+			else {
+				pushErrorLoading(line, "@cycle", "i");
+				return false;
+			}
+		}
+
+		else if (result = str_kit::lexicalAnalysis(line, "@counter", "i")) {
+			if (result == str_kit::LAR_valid) {
+				spec->target = TaSp_counter;
+				spec->cycle = str_kit::qStringToInt(line, 1);
+				return true;
+			}
+			else {
+				pushErrorLoading(line, "@counter", "i");
+				return false;
+			}
+		}
+
+		else {
+			err::logMessage("LOAD: Error loading file: " + file + " at line " + to_string(lineNumber) + ": unknown id");
+			return false;
+		}
+
+	}
+
+	//Takes a line and generates a ptr, null indicates the line could not be converted
+	Entity* createEnt(string line) {
+		//lex analysis for first template
+		if (str_kit::lexicalAnalysis(line, "ent", "i") == str_kit::LAR_valid) {
+			return new Entity(str_kit::qStringToInt(line, 1));
+		}
+		else if (str_kit::lexicalAnalysis(line, "ent", "s") == str_kit::LAR_valid) {
+			string token = str_kit::splitOnToken(line, ' ')[1];
+			if (stringToEntType(token) < 0) {
+				err::logMessage("LOAD: Error resolving token " + token + " to a recognized ent type at line " + to_string(lineNumber) + " in file " +  file);
+				return nullptr;
+			}
+			return new Entity(stringToEntType(token));
+		}
+		err::logMessage("LOAD: Error creating ent at" + to_string(lineNumber) + " in file " + file +
+			+"\n --> Expected: ent [float:ent_type]/[string:ent_type]");
+		return nullptr;
+	}
+
+	//Adds a component to the lates ent
+	bool addComponent(string line, Entity* ent) {
+		if not(ent) {
+			err::logMessage("LOAD: Error, attempting to add a component with no prior entity declaration " + to_string(lineNumber) + " in file " + file);
+			return false;
+		}
+		
+		//conduct lexical analysis
+		str_kit::LexicalAnalysisResult result;
+
+		//position
+		if (result = str_kit::lexicalAnalysis(line, "+position", "ff")) {
+
+			if (result == str_kit::LAR_valid) {
+				auto newComponent = new ComponentPosition(str_kit::qStringToFloat(line, 1), str_kit::qStringToFloat(line, 2), 0);
+				ent->addComponent( newComponent->pullForEntity() );
+				return true;
+			}
+
+			else if (result == str_kit::LAR_lexLengthFail) {
+				if (str_kit::lexicalAnalysis(line, "+position", "fff") == str_kit::LAR_valid) {
+					auto newComponent = new ComponentPosition(str_kit::qStringToFloat(line, 1), str_kit::qStringToFloat(line, 2), str_kit::qStringToFloat(line, 3));
+					ent->addComponent(newComponent->pullForEntity());
+					return true;
+				}
+			}
+
+			//at this point everything has failed, just return
+			err::logMessage("LOAD: Error, attempting to add a component with +position prefix" + to_string(lineNumber) + " in file " + file
+				+ "\n --> Expected: +Position [float:x] [float:y] [float:z=0]");
+			return false;
+		}
+
+		//graphics
+		else if (result = str_kit::lexicalAnalysis(line, "+graphics", "s")) {
+
+			if (result == str_kit::LAR_valid) {
+				auto newComponent = new ComponentGraphics(str_kit::splitOnToken(line, ' ')[1]);
+				ent->addComponent(newComponent->pullForEntity());
+				return true;
+			}
+
+			else {
+				err::logMessage("LOAD: Error, attempting to add a component with +graphics prefix" + to_string(lineNumber) + " in file " + file
+					+ "\n --> Expected: +graphics [string:set_name]");
+				return false;
+			}
+		}
+
+		err::logMessage("LOAD: Error, Unknown token used with \"+\" at " + to_string(lineNumber) + " in file " + file);
+		return false;
+	}
+
+	bool pushEnt(Entity* ent, TargetFullSpecification target) {
+		if (target.target == TaSp_noTarget) {
+			err::logMessage("LOAD: Error, Entity pushed at " + to_string(lineNumber) + " in file " + file + " with no target set prior");
+			return false;
+		}
+		else if (target.target == TaSp_cycle) {
+			cycleEnt[target.cycle] = ent;
+			return true;
+		}
+		return false;
+	}
+
 	bool parseLoadTable(string filepath) {
+		file = filepath;
+		lineNumber = -1;
+
+		TargetFullSpecification currentTarget;
+		Entity* ent = nullptr;
 
 		string line;
 		ifstream table(filepath);
-		while (getline(table, line)) {
+		bool valid = true;
+		while (getline(table, line) && valid) {
+			lineNumber++;
+			line = str_kit::trimString(line);
 			if (str_kit::isTrivialString(line)) {
 				continue;
 			}
 
-			
-
+			//check if start is a directive
+			if (line[0] == '@') {
+				valid = updateTarget(line, &currentTarget);
+			}
+			//else check if it spawns an ent
+			else if ( str_kit::compareStart("ent ", line) ) {
+				ent = createEnt(line);
+				valid = (bool)ent;
+				if (valid) {
+					valid = pushEnt(ent, currentTarget);
+				}
+			}
+			//else check if an component needs to be added'
+			else if (line[0] == '+') {
+				valid = addComponent(line, ent);
+			}
 
 		}
+
+		return valid;
 	}
 
 public:
@@ -126,7 +304,7 @@ public:
 		newEnt->addComponent(newComponent1->pullForEntity());
 		auto newComponent2 = new ComponentAnimation();
 		newEnt->addComponent(newComponent2->pullForEntity());
-		cycleEnt[3000] = newEnt;
+		//cycleEnt[3000] = newEnt;
 	}
 
 	void precycle(EntityPool* pool) override {
@@ -154,7 +332,10 @@ public:
 			return;
 		}
 
+		parseLoadTable(path);
+
 		registar->update("loading", false);
+		registar->update("cycle_progress", true);
 
 	}
 
