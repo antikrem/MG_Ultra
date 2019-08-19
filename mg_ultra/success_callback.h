7fdd3b5f3c33f3b2bc115fdc00668f17a60fac9b
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-08-19 16:19:16 +1000

    Added class to block thread until asyncronous sucess

---
+++ mg_ultra/success_callback.h
@@ -0,0 +1,38 @@
+/*Allows asyncronous tasks to be waited on until completion*/
+
+#include <atomic>
+#include <condition_variable>
+
+using namespace std;
+
+#ifndef __SUCCESS_CALLBACK__
+#define __SUCCESS_CALLBACK__
+
+class SuccessCallback {
+	//lock for cv
+	mutex lock;
+	condition_variable cv;
+
+	atomic<bool> complete = false;
+	bool result;
+
+public:
+
+	void setCompletion(bool successful) {
+		{
+			lock_guard<std::mutex> lk(lock);
+			result = successful;
+			complete = true;
+		}
+		cv.notify_one();
+	}
+
+	bool waitForCompletion() {
+		unique_lock<std::mutex> lk(lock);
+		cv.wait(lk, [this] {return this->complete.load(); });
+		return result;
+	}
+
+};
+
+#endif
