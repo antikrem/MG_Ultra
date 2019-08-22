63e4f580fcbc3c257a4df6d4a474ecde66c265ad
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-08-23 05:45:38 +1000

    Added method to read data from file as string easiliy

--- mg_ultra/os_kit.cpp
+++ mg_ultra/os_kit.cpp
@@ -1,6 +1,26 @@
 #include "os_kit.h"
 
+#include <fstream>
+
 bool os_kit::fileExists(const std::string& name) {
 	ifstream file(name.c_str());
 	return file.good();
+}
+
+string os_kit::getFileAsString(const std::string& filePath) {
+	ifstream in;
+	in.open(filePath);
+	if (!in) {
+		return string("");
+	}
+
+	string contents;
+	string line;
+	while (getline(in, line)) {
+		contents += line;
+		contents += '\n';
+	}
+	in.close();
+
+	return contents;
 }
--- mg_ultra/os_kit.h
+++ mg_ultra/os_kit.h
@@ -10,9 +10,12 @@ using namespace std;
 
 /*All methods are in the str_kit namespace*/
 namespace os_kit {
-
 	//checks if file exists
 	bool fileExists(const std::string& name);
+
+	//loads all the contents of a file from a script
+	//empty string on faliure
+	string getFileAsString(const std::string& filePath);
 }
 
 #endif
