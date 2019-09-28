9673a84b86a32b2eb13b44f39d8a9b2bb358ffde
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-09-28 15:07:57 +1000

    Events handled as queue of pointers

--- mg_ultra/events.cpp
+++ mg_ultra/events.cpp
@@ -1,27 +1,42 @@
 #include "events.h"
 
-#include <queue> 
+#include <queue>
+#include <atomic>
 #include <mutex>
+#include <condition_variable>
 
-queue<Event> eventQueue;
+queue<Event*> eventQueue;
+atomic<bool> pipelineOpen = true;
 mutex lck;
+condition_variable gate;
 
-void g_events::pushEvent(Event event) {
+void g_events::pushEvent(Event* event) {
 	unique_lock<mutex> l(lck);
+	if (!pipelineOpen) {
+		delete event;
+		return;
+	}
 	eventQueue.push(event);
+	l.unlock();
+	gate.notify_one();
 }
 
-bool g_events::pollEvents(Event* event) {
+bool g_events::pollEvents(Event** event) {
+	//wait for event to come in
 	unique_lock<mutex> l(lck);
-	if (!eventQueue.size()) {
-		return false;
-	}
+	gate.wait(l, [] { return g_events::queueSize(); });
 
-	*event = eventQueue.front();
+	Event* currentEvent = eventQueue.front();
+	*event = currentEvent;
 	eventQueue.pop();
 	return true;
 }
 
 int g_events::queueSize() {
 	return (int)eventQueue.size();
+}
+
+void g_events::closeEventPipeline() {
+	unique_lock<mutex> l(lck);
+	pipelineOpen = false;
 }
--- mg_ultra/events.h
+++ mg_ultra/events.h
@@ -14,9 +14,11 @@ enum EventType {
 	EV_noEvent,
 	EV_quit,
 	EV_loadNextState,
+	EV_invokeSystemMaster,
 	EV_LENGTH
 };
 
+/*Encapsulates an event, this can be thrown from anywhere in the engine*/
 struct Event {
 	EventType type;
 	vector<string> data;
@@ -25,14 +27,30 @@ struct Event {
 		this->type = type;
 	}
 
+	//create an event by reference
+	Event(const Event& event) {
+		this->type = event.type;
+		this->data = event.data;
+	}
+	
+	//create an event by pointer to another event
+	Event(const Event* event) {
+		this->type = event->type;
+		this->data = event->data;
+	}
 };
 
 namespace g_events {
+	/*Push the event onto the event que*/
+	void pushEvent(Event* event);
 
-	void pushEvent(Event event);
+	//blocks thread until a new event comes in
+	//updates event with pointer to new event
+	bool pollEvents(Event** event);
 
-	//on true, event will be populated with latest event
-	bool pollEvents(Event* event);
+	//closes event pipeline
+	//then clears all events in queue
+	void closeEventPipeline();
 
 	//debugging
 	int queueSize();
