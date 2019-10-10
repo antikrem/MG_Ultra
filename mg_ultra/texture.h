e07272928be66143bde234a5a15cc784c3e12f9c
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-10-10 23:37:35 +1000

    Changed default filter to linear

--- mg_ultra/texture.h
+++ mg_ultra/texture.h
@@ -108,8 +108,8 @@ public:
 		glActiveTexture(GL_TEXTURE0 + TU);
 		glBindTexture(GL_TEXTURE_2D, textureID);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
 	}
 
