4b8b28a8d475bac0656a9da063cbd3ee016b3bab
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-10-22 01:57:59 +1000

    Moved script location

---
+++ mg_ultra/wd/scripts/_library/library.lua
@@ -0,0 +1,108 @@
+---Basic functions, can be used in any script
+--No classes or mg functionality have been bounded, outside of global functions
+
+--gets length of vairable input/tables/etc
+function length(a)
+	count = 0
+	for i,v in pairs(a) do
+		count = count + 1
+	end
+	return count
+end
+
+function print_table(class)
+	for key,val in pairs(class) do
+		print(key, "=>", val);
+	end
+end
+
+--Variadic print to console/log
+function print(...) 
+	--get size of input
+	size = #{...}
+
+	--handle single input of type table, for doing things like printing classes
+	if size == 1 and type(select(1, ...)) == "table" then
+		print_table(select(1, ...))
+		return
+	end
+	
+	printAdd(">>")
+
+	--check for nil inputs
+	if size == 0 then
+		printAdd("nil")
+		printPush()
+		return
+	end
+
+	--otherwise general print
+	for i = 1,size do
+
+		v = select(i, ...)
+	
+		if v == nil then
+			printAdd("nil")
+		elseif type(v) == "userdata" and v.__name then
+			--v is a class
+			printAdd(v.__name)
+		else
+			printAdd(v)
+		end
+
+		printAdd(" ")
+	
+	end
+	printPush()
+end
+
+function typeof(a)
+	t = type(a)
+	--check if a is of userdata, and if so, check if it has a name
+	if t == "userdata" and a.__name then
+	    --t is a class
+		print(a.__name)
+		return
+	end
+	print(t)
+end
+
+--checks element is in the provided list of elements
+function check_element_in_set(element, ...)
+	for i = 1,#{...} do
+		if (element == select(i, ...)) then
+			return true;
+		end
+	end
+	return false;
+end
+
+--definitions for events
+EventNone = 0
+EventQuit = 1
+EventStateChange = 2
+EventCallback = 3
+
+--Emits an event to the event que, 
+function emit_event(eventType, ...)
+	--check event is valid
+	if not check_element_in_set(eventType, EventNone, EventQuit, EventStateChange, EventCallback) then
+		print("EVENT: error, invalid event type submitted with emit_event")
+		return
+	end
+
+	setEvent(eventType)
+	--set values
+	for i = 1,#{...} do
+		addEventData( select(i, ...) )
+	end
+
+	pushEvent()
+end
+
+--Requests engine shutdown by emmiting a quit event
+function quit()
+	print("Exiting from console...")
+	emit_event(EventQuit)
+end
+
---
+++ mg_ultra/wd/scripts/_library/library_ex.lua
@@ -0,0 +1,64 @@
+---Extended library
+--Classes and mg functionality have been bounded
+
+--global registar, keeps states
+registar = getGlobalRegistar()
+
+--Add a function within Entity class to allow components to be retrived 
+--by component name, returns nil if the entity does not have the specified component
+function Entity.get_component(self, component)
+	ptr = self:getComponent(component.type())
+	if ptr == nil then
+		return nil
+	else
+		return component.cast(ptr)
+	end
+end
+
+--add a function to Registar to allow weak typed Registar return
+--returns nil on failure
+function Registar.get(self, name)
+	--implementation is done by attempting different acessors
+	v, check = self:getInt(name)
+	if check then
+		return v
+	end
+
+	v, check = self:getFloat(name)
+	if check then
+		return v
+	end
+
+	v, check = self:getBool(name)
+	if check then
+		return v
+	end
+
+	v, check = self:getString(name)
+	if check then
+		return v
+	end
+
+	return nil
+end
+
+--general function to load a level by setting the correct values in the Registar
+--first parameter is state: title, level
+--second parameter is campaign name, valid on title state
+--third parameter is level number, valid on title state
+function load_state(...)
+	--Handling erreoneos event parameters will be handled by the engine's event handler
+	emit_event(EventStateChange, ...)
+end
+
+--Test
+function test() 
+	load_state("level", "ascent", 1)
+end
+
+
+function dump()
+	print("Event size:", get_event_length())
+	print("Entities size:", get_entity_count())
+	print("Memory Usage:", collectgarbage("count"))
+end
