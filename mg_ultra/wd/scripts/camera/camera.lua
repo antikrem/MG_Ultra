41e15fce15df44731dce75e6fb378dca96571057
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-11-02 03:39:11 +1000

    Camera doesn't leak components

--- mg_ultra/wd/scripts/camera/camera.lua
+++ mg_ultra/wd/scripts/camera/camera.lua
@@ -3,8 +3,8 @@
 --This script is run asyncronous to rendering
 
 --Pull components for the camera entity
-camPosition = this:get_component(ComponentPosition)
-camCamera = this:get_component(ComponentCamera)
+local camPosition = this:get_component(ComponentPosition)
+local camCamera = this:get_component(ComponentCamera)
 
 --Get the player entity and pull components
 local player = EntityPool.get_cached_entity(EntityPlayer)
@@ -12,7 +12,7 @@ local player = EntityPool.get_cached_entity(EntityPlayer)
 --if theres a player, then base camera on its position
 if not is_nil(player) then
     --set camera x,y position to the player
-    x,y = player:get_component(ComponentPosition):get_position()
-    camPosition:set_position(x, y)
-    camCamera:set_view_target(x, y)
+    local x, y = player:get_component(ComponentPosition):get_position()
+    camPosition:set_position(x, 0)
+    camCamera:set_view_target(x, 0)
 end
