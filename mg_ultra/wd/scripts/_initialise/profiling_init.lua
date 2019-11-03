128648811f9adda7e0921de54dc2e5055cdf4cbb
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-11-03 23:36:09 +1000

    Profiling format updated

--- mg_ultra/wd/scripts/_initialise/profiling_init.lua
+++ mg_ultra/wd/scripts/_initialise/profiling_init.lua
@@ -16,8 +16,9 @@ function convertToProfileString(name, slice)
 		--If theres a timing, we want to display :
 		-- 1. proportion of period spent executinf
 		-- 2. effective timing,
-		return name .. ": " 
-			.. "Float "
+		return 
+			string.pad_string(name, 12)
+			.. "F-> "
 			.. string.format("exec: %.3f ", slice[2]/slice[1]) 
 			.. string.format("etime: %d", math.floor(1000000 / (slice[2] + 1))) 
 			.. "\n"
@@ -26,8 +27,9 @@ function convertToProfileString(name, slice)
 		-- 1. proportion of period spent executing
 		-- 2. ratio of period to timing, as a percentage
 		local desiredTimePerCycle = 1000000 / slice[3]
-		return name .. ": " 
-			.. "Timed "
+		return 
+			string.pad_string(name, 12)
+			.. "T-> "
 			.. string.format("exec: %.3f ", slice[2]/slice[1]) 
 			.. string.format("speed: %3.2f%% ", 100*(slice[1] / desiredTimePerCycle) )
 			.. "\n"
