ff677da08a51d150911142712412231eb48235b0
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-11-03 17:42:44 +1000

    Updated player script to fire while held down

--- mg_ultra/wd/scripts/player/player.lua
+++ mg_ultra/wd/scripts/player/player.lua
@@ -9,14 +9,21 @@ PLAYER_MAX_VELOCITY = 5.0
 PLAYER_MAX_FOCUS_VELOCITY = 2.0
 PLAYER_MAX_DASH_VELOCITY = 15.0
 
+--shooting timer
+PLAYER_SHOOT_TIMING = 5
+
 --The position of the player
 local cPosition = this:get_component(ComponentPosition)
 --More sophisticated interface for controlling movement
 local cMovement = this:get_component(ComponentMovement)
+--Get graphics
+local cGraphics  = this:get_component(ComponentGraphics)
 --A component to look at movement
 local cInput = this:get_component(ComponentInput)
 --Extended scripting functionality
 local cEScript = this:get_component(ComponentExtendedScripting)
+--Timing component
+local cTiming = this:get_component(ComponentTimer)
 
 --add has no effect if value is already set
 --cEScript:add("test", 10)
@@ -46,7 +53,7 @@ end
 --angle will always point to input
 --magnitude increases linearly to max, and decays quickly with no input
 
-dmag, dang = to_polar( cMovement:get_velocity() )
+local dmag, dang = to_polar( cMovement:get_velocity() )
 
 --set magnitude and angle depending on input
 if (to_magnitude(ix,iy) < 0.1) then
@@ -60,16 +67,26 @@ dmag = clamp(dmag, 0, PLAYER_MAX_VELOCITY)
 cMovement:set_velocity( to_point(dmag, dang) )
 
 --create a bullet entity
-if cInput:query_press("shoot") == 1 then
-	cSpawner = this:get_component(ComponentSpawner)
-	cSpawner:create_entity(3)
-	cSpawner:add_component(ComponentPosition.create(x, y))
-	cSpawner:add_component(ComponentGraphics.create("default"))
-	cSpawner:push_entity()
+if cInput:query_down("shoot") == 1 and 
+		(cTiming:get_cycle() % PLAYER_SHOOT_TIMING) == 0 then
+    x, y = cPosition:get_position()
+	for bAngle in range(-45, 46, 15) do
+		cSpawner = this:get_component(ComponentSpawner)
+		cSpawner:create_entity(6)
+	
+		cSpawner:add_component(ComponentPosition.create(x, y))
+
+		local bulletGComponents = ComponentGraphics.create("default")
+		bulletGComponents:set_animation(2)
+		bulletGComponents:set_scale(0.1)
+		cSpawner:add_component(bulletGComponents)
+
+		local bulletMComponent = ComponentMovement.create()
+		bulletMComponent:set_speed(10.0)
+		bulletMComponent:set_angle(bAngle)
+		bulletMComponent:set_rotation_speed(5)
+		cSpawner:add_component(bulletMComponent)
+	
+		cSpawner:push_entity()
+	end
 end
-
-collectgarbage()
-
---create and collect 
---cSpawner = this:get_component(ComponentSpawner)
---typeof(cSpawner)
