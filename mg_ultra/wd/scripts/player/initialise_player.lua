ea4dab50fed087d8e30d8a022fecf8ce3773db01
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-11-09 00:29:20 +1000

    Updated the player scripts

--- mg_ultra/wd/scripts/player/initialise_player.lua
+++ mg_ultra/wd/scripts/player/initialise_player.lua
@@ -1,14 +1,19 @@
 --"this" refers to player
-print("Player_ini ran")
-
---some constant values
-WIDE_WIDTH = 1520
 
 --Player movement
 PLAYER_ACCELERATION = 0.57
-PLAYER_MAX_VELOCITY = 10.5
+PLAYER_MAX_VELOCITY = 7.65
 PLAYER_MAX_FOCUS_VELOCITY = 2.0
-PLAYER_MAX_DASH_VELOCITY = 15.0
+PLAYER_MAX_DASH_VELOCITY = 5.0
+--max turn rate of player 
+PLAYER_MAX_TURN_VELOCITY = 7;
+--Theres a mechanic called quick shift
+--if the player switches direction quickly its instant
+--This is the threshold to active
+PLAYER_QUICK_SHIFT_THRESHOLD = 70
 
 --shooting timer
 PLAYER_SHOOT_TIMING = 8
+
+this:get_component(ComponentMovement):set_speed_cap(PLAYER_MAX_VELOCITY)
+this:get_component(ComponentMovement):set_angle_cap(PLAYER_MAX_TURN_VELOCITY)
--- mg_ultra/wd/scripts/player/player.lua
+++ mg_ultra/wd/scripts/player/player.lua
@@ -30,27 +30,48 @@ if cInput:query_down("down") == 1 then
 	iy = iy - 1;
 end
 
---movement is calculated by treating velocity magnitude and angle seperatly
---angle will always point to input
---magnitude increases linearly to max, and decays quickly with no input
+--calculate updated movement
 
-local dmag, dang = math.to_polar( cMovement:get_velocity() )
+--current speedVelocity and speedAngle
+local smag, cang = cMovement:get_speed(), cMovement:get_angle()
+--calculate input direction
+local tmag, tang = math.to_polar(ix, iy)
 
---set magnitude and angle depending on input
-if (math.to_magnitude(ix,iy) < 0.1) then
-	dmag = dmag - PLAYER_ACCELERATION
+--need to turn compute shortest turn 
+local turn = math.compute_smallest_turn(tang, cang);
+
+if tmag > 0.1 then
+	--Theres a mechanic called quick shift
+	--if the player switches direction quickly its instant
+	if math.abs(turn) > PLAYER_QUICK_SHIFT_THRESHOLD then
+		cMovement:set_angle(tang)
+	else
+		cMovement:set_angle_change(turn)
+	end
 else
-	dmag = dmag + PLAYER_ACCELERATION
-	__, dang = math.to_polar(ix, iy)
+	cMovement:set_angle_change(0)
 end
-dmag = math.clamp(dmag, 0, PLAYER_MAX_VELOCITY)
 
-cMovement:set_velocity( math.to_point(dmag, dang) )
+--calculate acceleration
+--consider no input case
+if tmag < 0.1 then
+	--if speed is slow, set speed and acceleration to zero
+	if math.clamp(cMovement:get_speed(), 0, cMovement:get_speed()) <= PLAYER_ACCELERATION then
+		cMovement:set_speed(0.001)
+		cMovement:set_speed_change(0)
+		--otherwise, slow down the player
+	else 
+		cMovement:set_speed_change(-PLAYER_ACCELERATION)
+	end
+--otherwise set acceleration as expected
+else
+	cMovement:set_speed_change(PLAYER_ACCELERATION)
+end
 
 --create a bullet entity
 if cInput:query_down("shoot") == 1 and 
 		(cTiming:get_cycle() % PLAYER_SHOOT_TIMING) == 0 then
-    x, y = cPosition:get_position()
+    local x, y = cPosition:get_position()
 	for bAngle in range(-45, 46, 15) do
 		cSpawner = this:get_component(ComponentSpawner)
 		cSpawner:create_entity(6)
