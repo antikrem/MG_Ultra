1a8fa82d009d0781ead82fb78fe7b6d09f798d7f
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-11-25 23:47:24 +1000

    Added sub and pad

--- mg_ultra/wd/scripts/_library/string_ex.lua
+++ mg_ultra/wd/scripts/_library/string_ex.lua
@@ -2,8 +2,20 @@
 
 --pads a string with spaces until it reaches the given length
 --returns new string
-function string.pad_string(string, paddedLength)
+function string.pad_string(string, paddedLength, token)
+	token = token or " "
 	local padding = paddedLength - string.len(string)
+	padding = padding > 0 and padding or 0
 	padding = math.clamp(padding, 0, padding)
 	return string .. string.rep(" ", padding)
+end
+
+--takes a substring of a string from start to length
+function string.sub_string(str, length)
+	return string.sub(str, 0, length)
+end
+
+--combined substring and pad 
+function string.pad_sub(str, length, token)
+	return string.sub_string(string.pad_string(str, length, token), length)
 end
