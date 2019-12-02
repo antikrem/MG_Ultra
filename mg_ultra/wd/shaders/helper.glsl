40c7bb6f7f0461799d8eb63e9153682ff679cc3d
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-12-02 20:37:57 +1000

    Added angle functions

--- mg_ultra/wd/shaders/helper.glsl
+++ mg_ultra/wd/shaders/helper.glsl
@@ -10,6 +10,16 @@ float clampPositive(float value) {
 	return clamp(value, 0, abs(value));
 }
 
+/*computes the angle between u and v*/
+float cos_angle(vec3 u, vec3 v) {
+	return dot(normalize(u), normalize(v));
+}
+
+/*computes the angle between u and v*/
+float angle(vec3 u, vec3 v) {
+	return acos(cos_angle(u, v));
+}
+
 /*mixes proportions of a, b and c*/
 vec3 mix3(vec3 proportion, vec3 a, vec3 b, vec3 c) {
 	return proportion * transpose(mat3(a, b, c));
@@ -17,5 +27,5 @@ vec3 mix3(vec3 proportion, vec3 a, vec3 b, vec3 c) {
 
 /*mixes proportions of a, b and c, and fills the gap with base*/
 vec3 mix3(vec3 proportion, vec3 a, vec3 b, vec3 c, vec3 base) {
-	return  mix3(proportion, a, b, c) + clampPositive(1 - sum_up(proportion)) * base;
+	return mix3(proportion, a, b, c) + clampPositive(1 - sum_up(proportion)) * base;
 }
