19611cf2bafb88420bf7417c410bf0b9633d4601
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-12-22 23:03:33 +1000

    Added math_ex::tend_to

--- mg_ultra/math_ex.h
+++ mg_ultra/math_ex.h
@@ -3,11 +3,20 @@
 #define __MATH_EX__
 
 namespace math_ex {
-
 	//templated clamp
 	template <typename T>
-	T clamp(T value, T min, T max) {
+	T clamp(const T& value, const T& min, const T& max) {
 		return value > max ? max : (value < min ? min : value);
+	}
+
+	//templated tend to
+	//will tend current towards target at rate
+	//will not overshoort
+	//rate must be non-negative
+	template<typename T>
+	T tend_to(const T& current, const T& rate, const T& target) {
+		T difference = min(abs(target - current), rate);
+		return current > target ? current - difference : current + difference;
 	}
 }
 
