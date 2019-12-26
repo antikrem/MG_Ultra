f7046fa42c279bf77194063b5fb1678b5e2fb2fd
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-12-26 16:26:46 +1000

    Added thread safety to next randoms

--- mg_ultra/random_ex.cpp
+++ mg_ultra/random_ex.cpp
@@ -1,11 +1,15 @@
 #include <random>
+#include <mutex>
 
 #include "random_ex.h"
+
+std::mutex lock;
 
 std::default_random_engine rGen;
 std::normal_distribution<float> nDist(0.0f, 1.0f);
 
 
 float rand_ex::next_norm(float mu, float sigma) {
+	std::unique_lock<std::mutex> lck(lock);
 	return nDist(rGen) * sigma + mu;
 }
