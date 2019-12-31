5d579036e1ec9075c47effca36660a76cc3627d3
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2019-12-31 19:49:36 +1000

    Added registar values for windspeed

--- mg_ultra/wd/scripts/_initialise/drift.lua
+++ mg_ultra/wd/scripts/_initialise/drift.lua
@@ -1,8 +1,15 @@
 --Sets up some variables used for 
 --controlling drift from wind/level scroll
+
+--Scroll speed of level
 registar:add("drift_scrollspeed_x", 0.0)
 registar:add("drift_scrollspeed_y", 0.0)
 registar:add("drift_scrollspeed_z", 0.0)
+
+--Speed from wind
+registar:add("drift_windspeed_x", 0.0)
+registar:add("drift_windspeed_y", 0.0)
+registar:add("drift_windspeed_z", 0.0)
 
 Drift = {}	
 
@@ -14,4 +21,14 @@ function Drift.set_scroll_speed(x, y, z)
 	registar:update("drift_scrollspeed_x", x)
 	registar:update("drift_scrollspeed_y", y)
 	registar:update("drift_scrollspeed_z", z)
+end
+
+function Drift.set_wind_speed(x, y, z)
+	x = x or 1.0e-10
+	y = y or 1.0e-10
+	z = z or 1.0e-10
+
+	registar:update("drift_windspeed_x", x)
+	registar:update("drift_windspeed_y", y)
+	registar:update("drift_windspeed_z", z)
 end
