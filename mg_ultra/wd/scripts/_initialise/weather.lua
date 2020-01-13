5651824149c9ed8cc699c7077ec40ca348fa582b
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-01-13 16:41:35 +1000

    Moved camera and weather position

--- mg_ultra/wd/scripts/_initialise/weather.lua
+++ mg_ultra/wd/scripts/_initialise/weather.lua
@@ -9,10 +9,11 @@ registar:add("weather_type", WeatherTypes.None)
 
 --particles used for weather
 Particles.add_new_type("weather_heavyrain", "weather_rain0")
-Particles.set_box_dimension("weather_heavyrain", 400, 400, 400)
-Particles.set_box_center("weather_heavyrain", 0, 0, 400)
+Particles.set_box_dimension("weather_heavyrain", 960, 540, 750)
+Particles.set_box_center("weather_heavyrain", 0, 0, -100)
 Particles.set_weight("weather_heavyrain", 0.00, 0.00)
 Particles.set_type_response("weather_heavyrain", ParticleBoxResponse.Wrap)
+Particles.set_rotate_to_face("weather_heavyrain", true)
 
 --Assets for weather
 Audio.request_load_file("rain", "rain.wav")
@@ -22,7 +23,7 @@ Weather = {}
 --starts heavy rain
 Weather.start_heavy_rain = function()
 	registar:update("weather_type", WeatherTypes.HeavyRain)
-	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_uniformly(500)
-	Drift.set_wind_speed(-5, -15, 0)
+	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_uniformly(1000)
+	Drift.set_wind_speed(5, -15, 0)
 	Audio.play_track("rain")
 end
