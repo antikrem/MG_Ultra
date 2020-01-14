35bc5ae8ebe7a4bbee18e50b7a1dba7cc6e89a89
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-01-14 16:34:30 +1000

    Modified heavy rain parameter

--- mg_ultra/wd/scripts/_initialise/weather.lua
+++ mg_ultra/wd/scripts/_initialise/weather.lua
@@ -9,8 +9,8 @@ registar:add("weather_type", WeatherTypes.None)
 
 --particles used for weather
 Particles.add_new_type("weather_heavyrain", "weather_rain0")
-Particles.set_box_dimension("weather_heavyrain", 960, 540, 750)
-Particles.set_box_center("weather_heavyrain", 0, 0, -100)
+Particles.set_box_dimension("weather_heavyrain", 960, 540, 850)
+Particles.set_box_center("weather_heavyrain", 0, 0, -400)
 Particles.set_weight("weather_heavyrain", 0.00, 0.00)
 Particles.set_type_response("weather_heavyrain", ParticleBoxResponse.Wrap)
 Particles.set_rotate_to_face("weather_heavyrain", true)
@@ -24,6 +24,6 @@ Weather = {}
 Weather.start_heavy_rain = function()
 	registar:update("weather_type", WeatherTypes.HeavyRain)
 	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_uniformly(1000)
-	Drift.set_wind_speed(5, -15, 0)
+	Drift.set_wind_speed(14, -28, 0)
 	Audio.play_track("rain")
 end
