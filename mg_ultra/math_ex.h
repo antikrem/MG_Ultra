f10c30ad9c5850950cf79e94c99deb9738b118de
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-01-17 15:06:04 +1000

    Added quadratic solvers

--- mg_ultra/math_ex.h
+++ mg_ultra/math_ex.h
@@ -3,6 +3,11 @@
 #define __MATH_EX__
 
 #include <algorithm>
+#include <tuple>
+
+#include "constants.h"
+
+using namespace std;
 
 namespace math_ex {
 	//templated clamp
@@ -17,7 +22,7 @@ namespace math_ex {
 	//rate must be non-negative
 	template<typename T>
 	T tend_to(const T& current, const T& rate, const T& target) {
-		T difference = std::min(abs(target - current), rate);
+		T difference = min(abs(target - current), rate);
 		return current > target ? current - difference : current + difference;
 	}
 
@@ -26,6 +31,28 @@ namespace math_ex {
 
 	//computes shortest turn to target from current
 	float compute_smallest_turn(float target, float current);
+
+	//solves quadratic equation
+	template<typename T>
+	tuple<T, T> solve_quadratic(T a, T b, T c) {
+		T sqrtDisc = sqrt(SQUARE(b) - ((T)4) * a * c);
+
+		return make_tuple(
+			(-b - sqrtDisc) / (2 * a),
+			(-b + sqrtDisc) / (2 * a)
+		);
+	}
+
+	//solves quadratic given y
+	template<typename T>
+	tuple<T, T> solve_quadratic(T a, T b, T c, T y) {
+		T sqrtDisc = sqrt(SQUARE(b) - ((T)4) * a * (c-y));
+
+		return make_tuple(
+			(-b - sqrtDisc) / (2 * a),
+			(-b + sqrtDisc) / (2 * a)
+		);
+	}
 }
 
 #endif
