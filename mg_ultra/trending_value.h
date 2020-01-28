c2c47b514976b846354699a60332b85430ab4e14
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-01-29 00:59:54 +1000

    Added trending_value class

---
+++ mg_ultra/trending_value.h
@@ -0,0 +1,58 @@
+/*A class for values that utilise
+a current value, target and some rate of change*/
+#ifndef __TRENDING_VALUE__
+#define __TRENDING_VALUE__
+
+#include <atomic>
+
+#include "math_ex.h"
+
+template <class T>
+class TrendingValue {
+	std::atomic<T> current;
+
+	std::atomic<T> target;
+
+	std::atomic<T> rate;
+
+
+public:
+	TrendingValue(T value)
+		: current(value), target(value), rate((T)1) {
+
+	}
+
+	TrendingValue(T value, T rate)
+		: current(value), target(value), rate(rate) {
+
+	}
+
+	TrendingValue(T current, T rate, T target)
+		: current(current), target(target), rate(rate) {
+
+	}
+
+	void update() {
+		current = math_ex::tend_to(current.load(), rate.load(), target.load());
+	}
+
+	T get() {
+		return current;
+	}
+
+	void setCurrent(T current) {
+		this->current = current;
+	}
+
+	void setTarget(T target) {
+		this->target = target;
+	}
+
+	void setRate(T rate) {
+		this->rate = rate;
+	}
+
+};
+
+
+#endif
