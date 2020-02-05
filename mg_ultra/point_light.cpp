2cfdb4b4cffc4354d86923af96ed98345a84136a
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-02-05 18:15:09 +1000

    Added factor for drifting point light

--- mg_ultra/point_light.cpp
+++ mg_ultra/point_light.cpp
@@ -5,14 +5,16 @@
 
 #define PERLIN_TEXTURE_WIDTH 1024.0f
 
+atomic<float> pointLightDriftFactor = 0.8f;
+
 mutex gPointLightLock;
 Point2 combinedDrift = Point2(0.0f);
 
 void g_pointlights::updateDrift(const Point2& shift) {
 	unique_lock<mutex> lck(gPointLightLock);
 	combinedDrift = Point2(
-		fmod(combinedDrift.x + shift.x, PERLIN_TEXTURE_WIDTH),
-		fmod(combinedDrift.y + shift.y, PERLIN_TEXTURE_WIDTH)
+		fmod(combinedDrift.x + pointLightDriftFactor * shift.x, PERLIN_TEXTURE_WIDTH),
+		fmod(combinedDrift.y + pointLightDriftFactor * shift.y, PERLIN_TEXTURE_WIDTH)
 	);
 }
 
