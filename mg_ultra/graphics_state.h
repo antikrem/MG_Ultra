93f98808011d861380005ab661f1b974806f1df8
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-02-06 06:08:15 +1000

    Removed msaa

--- mg_ultra/graphics_state.h
+++ mg_ultra/graphics_state.h
@@ -31,7 +31,6 @@ private:
 			return EXIT_FAILURE;
 		}
 
-		glfwWindowHint(GLFW_SAMPLES, 4);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
