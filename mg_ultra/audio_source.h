074b127da203caacc485ee74a625811055cd4876
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-02-11 03:23:07 +1000

    Added better control over audio playing

--- mg_ultra/audio_source.h
+++ mg_ultra/audio_source.h
@@ -38,6 +38,10 @@ private:
 	string currentAudio;
 	string requestedAudio;
 
+	bool requestPlay = false;
+	bool requestStop = false;
+
+
 	//id of source
 	ALuint sourceID;
 public:
@@ -50,10 +54,28 @@ public:
 		}
 	}
 
-	//plays a sound through this audio source
-	void playAudio(const string& audioName) {
+	//sets a sound file as what will be played
+	void setQueuedAudio(const string& audioName) {
 		unique_lock<mutex> lck(lock);
 		requestedAudio = audioName;
+	}
+
+	//played the queued audio
+	void playQueuedAudio() {
+		unique_lock<mutex> lck(lock);
+		requestPlay = true;
+	}
+
+	//plays a sound through this audio source
+	void playAudio(const string& audioName) {
+		setQueuedAudio(audioName);
+		playQueuedAudio();
+	}
+
+	//stops audio
+	void stopAudio() {
+		unique_lock<mutex> lck(lock);
+		requestStop = true;
 	}
 
 	//sets this source to a certain repeat value
@@ -91,22 +113,32 @@ public:
 			requestedGain = -1.0f;
 		}
 
-		//if a requested audio is in, siwtch to it
+		//if a requested audio is in, switch to it
 		if (requestedAudio.size()) {
 			//check if requested audio is availible
 			if (audioFiles.count(requestedAudio)) {
 				//get new bufferID to play
 				int bufferID = audioFiles[requestedAudio]->getBufferID();
 				alSourcei(sourceID, AL_BUFFER, bufferID);
-				alSourcePlay(sourceID);
 				currentAudio = requestedAudio;
 			}
 			else {
 				err::logMessage("AUDIO: Error, could not find requested audio: " + requestedAudio);
 			}
 
-			
 			requestedAudio = "";
+		}
+
+		//if a requested audio has come in
+		if (requestPlay) {
+			requestPlay = false;
+			alSourcePlay(sourceID);
+		}
+
+		//if a requested stop has come in
+		if (requestStop) {
+			requestStop = false;
+			alSourceStop(sourceID);
 		}
 
 	}
--- mg_ultra/component_audio.h
+++ mg_ultra/component_audio.h
@@ -1,6 +1,8 @@
 #ifndef __COMPONENT_AUDIO__
 #define __COMPONENT_AUDIO__
 
+#include <atomic>
+
 #include "component.h"
 #include "constants.h"
 #include "scriptable_class.h"
@@ -9,16 +11,43 @@
 class ComponentAudio : public Component, public ScriptableClass {
 private:
 	AudioSource source;
+
+	//set to true if there was a play request
+	//that would be able to be stopped
+	//consider it a neccesary (but not sufficient) condition
+	//that audio is currently playing
+	atomic<bool> playRequested = false;
 public:
 
 	ComponentAudio() {
 
 	}
 
+	//sets to play a audio file
+	void setSource(const string& audioName) {
+		source.setQueuedAudio(audioName);
+	}
+
+	void playSource() {
+		source.playQueuedAudio();
+		playRequested = true;
+	}
+
 	//plays an audio file
 	void playAudio(const string& audioName) {
 		source.playAudio(audioName);
+		playRequested = true;
 	}
+
+	//stop audio being played
+	void stopAudio() {
+		if (playRequested) {
+			source.stopAudio();
+			playRequested = false;
+		}
+	}
+
+	
 
 	//sets the repeat value
 	void setRepeat(bool repeat) {
@@ -39,6 +68,10 @@ public:
 		state["ComponentAudio"].setClass(kaguya::UserdataMetatable<ComponentAudio, Component>()
 			.setConstructors<ComponentAudio()>()
 			.addFunction("play", &ComponentAudio::playAudio)
+			.addFunction("play_source", &ComponentAudio::playSource)
+			.addFunction("stop", &ComponentAudio::stopAudio)
+			.addFunction("stop_source", &ComponentAudio::stopAudio)
+			.addFunction("set_source", &ComponentAudio::setSource)
 			.addFunction("set_repeat", &ComponentAudio::setRepeat)
 			.addFunction("set_gain", &ComponentAudio::setGain)
 			.addOverloadedFunctions(
