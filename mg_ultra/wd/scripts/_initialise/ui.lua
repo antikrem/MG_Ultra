557318133a08f95ec097d1db7814b667e3e10bda
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-02-17 19:29:54 +1000

    Added transparency for ui

--- mg_ultra/wd/scripts/_initialise/ui.lua
+++ mg_ultra/wd/scripts/_initialise/ui.lua
@@ -18,3 +18,19 @@ end
 UI.remove_cinematic_borders = function()
 	UI.set_border(1.0, 0.009)
 end
+
+UI.display_title_card = function(titleSet, fadeIn, delay, fadeOut)
+	local e = Entity.create(EntityGeneric)
+	e:add_component(ComponentPosition.create(0,0,-1))
+	e:add_component(ComponentTransparency.create(1.0, 1.0 / fadeIn))
+
+	local gra = ComponentGraphics.create(titleSet)
+	gra:set_render_in_3D(false)
+	e:add_component(gra)
+	
+	
+
+	EntityPool.add_entity(e)
+
+
+end
