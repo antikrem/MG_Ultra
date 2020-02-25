755a07edb9e926aaae036aea12bde537521e3efd
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-02-26 00:24:07 +1000

    Added some audio and expanded lightning

--- mg_ultra/wd/scripts/_initialise/weather.lua
+++ mg_ultra/wd/scripts/_initialise/weather.lua
@@ -9,14 +9,24 @@ registar:add("weather_type", WeatherTypes.None)
 
 --particles used for weather
 Particles.add_new_type("weather_heavyrain", "weather_rain0")
-Particles.set_box_dimension("weather_heavyrain", 960, 540, 800)
-Particles.set_box_center("weather_heavyrain", 0, 0, -400)
+Particles.set_box_dimension("weather_heavyrain", 960, 540, 700)
+Particles.set_box_center("weather_heavyrain", 0, 0, -160)
 Particles.set_weight("weather_heavyrain", 0.00, 0.00)
 Particles.set_type_response("weather_heavyrain", ParticleBoxResponse.Wrap)
 Particles.set_rotate_to_face("weather_heavyrain", true)
 
+Particles.add_new_type("weather_darkcloud", "weather_cloud0", 1, 2.4)
+Particles.set_box_dimension("weather_darkcloud", 1300, 1400, 490)
+Particles.set_box_center("weather_darkcloud", 0, 0, 500)
+Particles.set_featherness("weather_darkcloud", 0.00, 0.00)
+Particles.set_scrollness("weather_darkcloud", 1)
+Particles.set_type_response("weather_darkcloud", ParticleBoxResponse.Wrap)
+
 --Assets for weather
 Audio.request_load_file("rain", "rain.wav")
+Audio.request_load_file("thunder_strike0", "strike0.wav")
+Audio.request_load_file("thunder_rumble0", "thunder0.wav")
+Audio.request_load_file("thunder_rumble1", "thunder1.wav")
 
 Weather = {}
 
@@ -27,6 +37,9 @@ Weather.start_heavy_rain = function()
 
 	--Start rain
 	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_uniformly(900)
+	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):use_type("weather_darkcloud")
+	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_uniformly(7)
+	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentParticle):spawn_at(0, 0, 10)
 	EntityPool.get_cached_entity(EntityWeather):get_component(ComponentAudio):play("rain")
 	Drift.set_wind_speed(10, -24, 0)
 
@@ -46,7 +59,7 @@ end
 Weather.update_player_light = function()
 	local p = EntityPool.get_player() and EntityPool.get_player() or this
 	if p and registar:get("weather_type") == WeatherTypes.HeavyRain then 
-		p:get_component(ComponentPointLight):set_parameters(0.00078, 0, 2) 
+		p:get_component(ComponentPointLight):set_parameters(0.00068, 0, 1.5) 
 	end
 end
 
@@ -85,13 +98,14 @@ Weather.strike_weak_lightning = function(delay, fadeout, pX, pY, strength, r, g,
 	local d = ComponentDirectionalLight.create(pX, pY, -1.0, r, g, b)
 	d:set_strength(strength)
 	e:add_component(d)
-	pool:add(e)
+
+	EntityPool.add_entity(e)
 end
 
 -- Strikes lightning
 Weather.strike_lightning = function(bursts, pX, pY)
-	local delay = 5
-	local fadeout = 15
+	local delay = 10
+	local fadeout = 8
 
 	if is_nil(bursts) then bursts = 3 end
 	if is_nil(pX) then pX = 1 end
@@ -115,9 +129,10 @@ Weather.strike_lightning = function(bursts, pX, pY)
 		"PostEffects.Bloom.set_threshold(0.2)" 
 	)
 	e:add_component(c)
-	local d = ComponentDirectionalLight.create(pX, pY, -1.0, 1.0, 1.0, 1.0)
+	local d = ComponentDirectionalLight.create(pX, pY, -1.0, 2.0, 2.0, 2.0)
 	e:add_component(d)
-	pool:add(e)
+
+	EntityPool.add_entity(e)
 end
 
 -- Applies a blinding strike of lightning
@@ -125,7 +140,7 @@ Weather.strike_blinding_lightning = function(delay, fadeout)
 	if is_nil(delay) then delay = 100 end
 	if is_nil(fadeout) then fadeout = 100 end
 
-	ColourModulation.set_cutoff(1070)
+	ColourModulation.set_cutoff(650)
 	ColourModulation.set_background_colour(100000, 100000, 100000)
 	ColourModulation.set_foreground_colour(0,0,0)
 	ColourModulation.set_strength(1.2)
@@ -141,11 +156,94 @@ Weather.strike_blinding_lightning = function(delay, fadeout)
 		"this:get_component(ComponentDirectionalLight):set_strength(1.0, " .. tostring(1.0 / fadeout) .. ", 0)" 
 	)
 	c:add_callback(
-		delay + fadeout, 
+		math.max(delay + fadeout, 1000), 
 		"this:kill()"
 	)
 	e:add_component(c)
 	local d = ComponentDirectionalLight.create(1.0, 0, -1.0, 1.0, 1.0, 1.0)
 	e:add_component(d)
-	pool:add(e)
+	e:add_component(ComponentAudio.create("thunder_strike0"))
+
+	EntityPool.add_entity(e)
+end
+
+--Strike lightning as a flash of powerful point lightning
+Weather.strike_point_lightning = function(x, y, z, delay, fadeout, r, g, b, strength)
+	
+	local e = Entity.create(EntityGeneric)
+
+	local c = ComponentTimer.create()
+
+	--Add each burst
+	c:add_callback(
+		delay,
+		"this:get_component(ComponentPointLight):set_strength(0, " .. tostring(strength / fadeout) .. ")" 
+	)
+	c:add_callback(
+		delay + fadeout, 
+		"this:kill()"
+	)
+	e:add_component(c)
+	
+	local p = ComponentPointLight.create(r, g, b, 0.00003, 0.001, 2)
+	e:add_component(p)
+	local p = ComponentPosition.create(x, y, z)
+	e:add_component(p)
+	e:add_component(ComponentNoBoundsControl.create())
+
+	EntityPool.add_entity(e)
+end
+
+--Strike lightning as a flash of powerful point lightning randomly
+Weather.strike_random_point_lightning = function()
+	
+	local x,y = math.rand_vec(2, -1000, 1000)
+	local z = math.lerp(math.random(), 500, 700)
+	Weather.strike_point_lightning(
+		x,y,z,
+		10,
+		2,
+		1.0,1.0,1.0,
+		1.0
+	)
+end
+
+--Strike lightning as a burst of multiple lights
+Weather.strike_burst_lightning = function()
+	
+	local e = Entity.create(EntityGeneric)
+	local c = ComponentTimer.create()
+	c:add_callback(1, "Weather.strike_random_point_lightning()")
+	c:add_callback(math.random(5, 9), "Weather.strike_random_point_lightning()")
+	if math.random() > 0.5 then
+		c:add_callback(math.random(12, 17), "Weather.strike_random_point_lightning()")
+	end
+	c:add_callback(100, "this:kill()")
+	e:add_component(c) 
+	e:add_component(ComponentAudio.create("thunder_rumble0"))
+
+	EntityPool.add_entity(e)
+end
+
+--Strike lightning as a burst of multiple lights, with heavy sound
+Weather.strike_burst_lightning_alt = function()
+	
+	local e = Entity.create(EntityGeneric)
+	local c = ComponentTimer.create()
+	c:add_callback(1, "Weather.strike_random_point_lightning()")
+	c:add_callback(math.random(5, 9), "Weather.strike_random_point_lightning()")
+	if math.random() > 0.5 then
+		c:add_callback(math.random(12, 17), "Weather.strike_random_point_lightning()")
+	end
+	c:add_callback(500, "this:kill()")
+	e:add_component(c) 
+	e:add_component(ComponentAudio.create("thunder_rumble1"))
+
+	EntityPool.add_entity(e)
+end
+
+--Strike lightning as burst with a bit of a blind
+Weather.strike_blinding_burst_light = function()
+	Weather.strike_burst_lightning_alt()
+	Weather.strike_lightning()
 end
