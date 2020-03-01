06983c26b505883fd155e80b3ef34d31a1e41d8c
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-03-01 16:54:17 +1000

    Improved and expanded string.pad_string

--- mg_ultra/wd/scripts/_library/string_ex.lua
+++ mg_ultra/wd/scripts/_library/string_ex.lua
@@ -2,12 +2,17 @@
 
 --pads a string with spaces until it reaches the given length
 --returns new string
-function string.pad_string(string, paddedLength, token)
-	token = token or " "
+function string.pad_string(string, paddedLength, token, reverse)
+	if is_nil(token) then token = " " end
+	if is_nil(reverse) then reverse = false end
 	local padding = paddedLength - string.len(string)
 	padding = padding > 0 and padding or 0
 	padding = math.clamp(padding, 0, padding)
-	return string .. string.rep(" ", padding)
+	if reverse then
+		return string.rep(token, padding) .. string
+	else
+		return string .. string.rep(token, padding)
+	end
 end
 
 --takes a substring of a string from start to length
