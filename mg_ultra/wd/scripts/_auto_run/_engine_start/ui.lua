74649c30c7925cc7826211413cd0877c7a5aada2
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-03-02 02:44:07 +1000

    Updated script location

--- mg_ultra/wd/scripts/_auto_run/_engine_start/ui.lua
+++ mg_ultra/wd/scripts/_auto_run/_engine_start/ui.lua
@@ -1,4 +1,4 @@
---\scripts\_initialise\ui.lua
+-- \scripts\_auto_run\_engine_start\ui.lua
 
 -- Namespace for ui modification
 UI = {}
@@ -49,4 +49,81 @@ UI.display_title_card = function(titleSet, fadeIn, delay, fadeOut, scale)
 	EntityPool.add_entity(e)
 
 
+end
+
+-- False when ui hasn't spawned yet
+UI.uiCreated = false
+
+-- True when ui is being drawn
+UI.uiVisible = false
+
+UI.start_ui = function() 
+	local e = Entity.create(EntityScoreBoard)
+	e:add_component(ComponentPosition.create(-900, 500))
+	local c = ComponentText.create("text_consolas58")
+	c:set_render_in_3D(false)
+	c:set_text("000000000")
+	c:set_visible(false)
+	e:add_component(c)
+
+	EntityPool.add_cached_entity(e)
+
+	local e = Entity.create(EntityFragmentBoard)
+	e:add_component(ComponentPosition.create(-900, 450))
+	local c = ComponentText.create("text_consolas58")
+	c:set_render_in_3D(false)
+	c:set_text(tostring(g_fragments) .. "/" .. tostring(g_nextFragments))
+	c:set_visible(false)
+	e:add_component(c)
+
+	EntityPool.add_cached_entity(e)
+
+	local e = Entity.create(EntityPowerBoard)
+	e:add_component(ComponentPosition.create(-900, 400))
+	local c = ComponentText.create("text_consolas58")
+	c:set_render_in_3D(false)
+	c:set_text("00.00%")
+	c:set_visible(false)
+	e:add_component(c)
+
+	EntityPool.add_cached_entity(e)
+
+	UI.uiCreated = true
+end
+
+UI.show_ui = function()
+	local e = EntityPool.get_cached_entity(EntityScoreBoard)
+	e:get_component(ComponentText):set_visible(true)
+
+	local e = EntityPool.get_cached_entity(EntityFragmentBoard)
+	e:get_component(ComponentText):set_visible(true)
+
+	local e = EntityPool.get_cached_entity(EntityPowerBoard)
+	e:get_component(ComponentText):set_visible(true)
+
+	UI.uiVisible = true
+end
+
+UI.hide_ui = function()
+	local e = EntityPool.get_cached_entity(EntityScoreBoard)
+	e:get_component(ComponentText):set_visible(false)
+
+	local e = EntityPool.get_cached_entity(EntityFragmentBoard)
+	e:get_component(ComponentText):set_visible(false)
+
+	local e = EntityPool.get_cached_entity(EntityPowerBoard)
+	e:get_component(ComponentText):set_visible(false)
+
+	UI.uiVisible = false
+end
+
+UI.update_ui = function()
+	local e = EntityPool.get_cached_entity(EntityScoreBoard)
+	e:get_component(ComponentText):set_text(string.pad_string(tostring(g_points), 9, "0", true))
+
+	local e = EntityPool.get_cached_entity(EntityFragmentBoard)
+	e:get_component(ComponentText):set_text(tostring(g_fragments) .. "/" .. tostring(g_nextFragments))
+
+	local e = EntityPool.get_cached_entity(EntityPowerBoard)
+	e:get_component(ComponentText):set_text(tostring(g_power) .. "%")
 end
