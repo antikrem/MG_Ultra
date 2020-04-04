074acb693e5ba5965ce953d341f37d1698b9c398
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-04-04 16:46:11 +1000

    Improved lua random number performance

--- mg_ultra/wd/scripts/_auto_run/_engine_start/seed_random.lua
+++ mg_ultra/wd/scripts/_auto_run/_engine_start/seed_random.lua
@@ -1,2 +1,7 @@
---Initialise random
+-- \scripts\_auto_run\_engine_start\seed_random.lua
+
+-- Initialise random
 math.randomseed(os.time())
+
+-- Pop some random numbers
+for i = 1,10 do math.random() end
