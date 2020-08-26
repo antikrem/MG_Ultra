57e7ac37dcf55c87d8fd8fef0969a386b2e067c4
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-08-27 03:32:13 +1000

    Added to_keys

--- mg_ultra/algorithm_ex.h
+++ mg_ultra/algorithm_ex.h
@@ -1,5 +1,6 @@
-/*Extends algorthm library
-Including range based algorithm functions*/
+/* Extends algorthm library
+ * Including range based algorithm functions
+ */
 
 #ifndef __ALGORITHM_EX__
 #define __ALGORITHM_EX__
@@ -8,12 +9,14 @@ Including range based algorithm functions*/
 #include <functional>
 #include <type_traits>
 #include <stack>
+#include <map>
+#include <set>
 #include <cstdint>
 
 using namespace std;
 
-//erase if for sequential STL containers
-//removes on true
+// Erase if for sequential STL containers
+// Removes on true
 template <class Container>
 void erase_sequential_if(Container& container, const function<bool(typename Container::value_type&)> conditional) {
 	//utilise erase/remove_if
@@ -27,8 +30,8 @@ void erase_sequential_if(Container& container, const function<bool(typename Cont
 	);
 }
 
-//erase if for associative STL containers
-//conditional will delete on true
+// Erase if for associative STL containers
+// Conditional will delete on true
 template <class Container>
 void erase_associative_if(Container& container, const function<bool(typename Container::value_type&)> conditional) {
 	//use a while loop
@@ -43,27 +46,27 @@ void erase_associative_if(Container& container, const function<bool(typename Con
 	}
 }
 
-//extends a sequential container with another
+// Extends a sequential container with another
 template<class Container1, class Container2>
 void extend(Container1& container, Container2& extension) {
 	container.insert(container.end(), extension.begin(), extension.end());
 }
 
-//Checks if container contains element
+// Checks if container contains element
 template <class Container>
 bool contains(const Container& container, const typename Container::value_type& element) {
 	return std::find(container.begin(), container.end(), element) != container.end();
 }
 
-//finds the index of a given value in a container
-//returns -1 on not finding element
+// Finds the index of a given value in a container
+// Returns -1 on not finding element
 template <class Container>
 int index_of(const Container& container, const typename Container::value_type& element) {
 	auto it = std::find(container.begin(), container.end(), element);
 	return it != container.end() ? distance(container.begin(), it) : -1;
 }
 
-//replaces the value of a container
+// Replaces the value of a container
 template <class Container>
 void replace(Container& container, const typename Container::value_type& element, const typename Container::value_type& replacement) {
 	auto it = std::find(container.begin(), container.end(), element);
@@ -73,7 +76,7 @@ void replace(Container& container, const typename Container::value_type& element
 	}
 }
 
-//sorts a container
+// Sorts a container
 template <class Container>
 void sort(Container& container,
 		const function<bool(typename Container::value_type&, typename Container::value_type&)> comparator
@@ -82,7 +85,7 @@ void sort(Container& container,
 	
 }
 
-//deletes if not null
+// Deletes if not null
 template <class T>
 void delete_or_null(T* &object) {
 	if (object) {
@@ -91,8 +94,8 @@ void delete_or_null(T* &object) {
 	}
 }
 
-//deletes everything inside a stack
-//TODO assert type
+// Deletes everything inside a stack
+// TODO assert type
 template <class T>
 void clear_stack(T &s) {
 	while (!s.empty()) {
@@ -100,10 +103,10 @@ void clear_stack(T &s) {
 	}
 }
 
-//compares both pointers
-//if both are null, returns null
-//if one is null, returns other
-//if both aren't null returns a
+// Compares both pointers
+// If both are null, returns null
+// If one is null, returns other
+// If both aren't null returns a
 template <class T>
 T* ptr_or(T* a, T* b) {
 	if (a && b) {
@@ -114,5 +117,19 @@ T* ptr_or(T* a, T* b) {
 		| reinterpret_cast<std::uintptr_t>(b)
 	);
 }
+
+// Returns set of keys to a map
+template<class T, class D>
+set<T> to_keys(map<T, D> container) {
+	set<T> result;
+	transform(
+			container.begin(), 
+			container.end(),
+			inserter(result, result.end()),
+			[](auto pair) 
+				{ return pair.first; }
+		);
+}
+
 
 #endif
