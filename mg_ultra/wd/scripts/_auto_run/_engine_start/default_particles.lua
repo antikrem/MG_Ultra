031d9e523eb087afa2c1823cdd7587c4a67b7055
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-11-20 11:22:35 +1000

    Added death_gold_fragments_subtle

--- mg_ultra/wd/scripts/_auto_run/_engine_start/default_particles.lua
+++ mg_ultra/wd/scripts/_auto_run/_engine_start/default_particles.lua
@@ -12,8 +12,14 @@ Particles.set_max_life_deviation("gold", 3.0)
 Particles.set_featherness("gold", 0.05, 0.025)
 
 Particles.add_new_type("death_gold_fragments", "particle_gold_frag")
-Particles.set_bloom_factor("death_gold_fragments", 4.0)
-Particles.set_max_life_deviation("death_gold_fragments", 3.5)
+Particles.set_bloom_factor("death_gold_fragments", 3.0)
+Particles.set_max_life_deviation("death_gold_fragments", 2.5)
 Particles.set_featherness("death_gold_fragments", 0.012, 0.025)
 Particles.set_rotate_to_face("death_gold_fragments", true)
+
+Particles.add_new_type("death_gold_fragments_subtle", "particle_gold_subtle")
+Particles.set_bloom_factor("death_gold_fragments_subtle", 3.0)
+Particles.set_max_life_deviation("death_gold_fragments_subtle", 2.5)
+Particles.set_featherness("death_gold_fragments_subtle", 0.012, 0.025)
+Particles.set_rotate_to_face("death_gold_fragments_subtle", true)
 
