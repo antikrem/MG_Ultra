84ba1438a880c2a661f5d23935dc6fa0c39a5364
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2020-12-26 02:11:52 +1000

    Added clear enemies script

--- mg_ultra/wd/scripts/_auto_run/_engine_start/gameplay.lua
+++ mg_ultra/wd/scripts/_auto_run/_engine_start/gameplay.lua
@@ -3,7 +3,12 @@
 -- Manages some functions to modify gameplay
 Gameplay = {}
 
+-- Deletes all bullets
 Gameplay.clear_bullets = function()
 	EntityPool.kill_by_type(EntityEnemyBullet)
 end
 
+-- Deletes all enemies
+Gameplay.clear_enemies = function()
+	EntityPool.kill_by_type(EntityEnemy)
+end
