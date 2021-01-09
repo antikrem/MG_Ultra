21bdbaf2a840ca95402b84c790d6f3344c1a91c6
Chalinda Dinusha Rodrigo <chalindarodrigo@gmail.com>
2021-01-10 02:35:43 +1000

    Added Extend mode to gameplay

--- mg_ultra/wd/scripts/_auto_run/_engine_start/gameplay.lua
+++ mg_ultra/wd/scripts/_auto_run/_engine_start/gameplay.lua
@@ -3,6 +3,11 @@
 -- Manages some functions to modify gameplay
 Gameplay = {}
 
+-- Plays spaces in different gameplay modes
+Gameplay.HEIGHT = 1080 / 2
+Gameplay.GENERAL_WIDTH = 1440 / 2
+Gameplay.EXTEND_WIDTH = 1920 / 2
+
 -- Deletes all bullets
 Gameplay.clear_bullets = function()
 	EntityPool.kill_by_type(EntityEnemyBullet)
@@ -11,4 +16,25 @@ end
 -- Deletes all enemies
 Gameplay.clear_enemies = function()
 	EntityPool.kill_by_type(EntityEnemy)
+end
+
+-- Extends gamplay arena
+Gameplay.extend = function(frames)
+	if is_nil(frames) then frames = 200 end
+	local rate = tostring(1.0/frames)
+
+	UI.hide_panes(frames)
+	local p = EntityPool.get_player()
+
+	if not is_nil(p) then
+		p:get_component(ComponentClampPosition):set_dimensions(Gameplay.EXTEND_WIDTH, Gameplay.EXTEND_WIDTH)
+	end
+	
+	local e = Entity.create(EntityUIElement)
+	e:add_component(ComponentPosition.create(0,0,0))
+	local t = ComponentText.create("text_consolas58")
+	t:set_text("Extend")
+	e:add_component(t)
+
+	EntityPool.add_entity(e)
 end
